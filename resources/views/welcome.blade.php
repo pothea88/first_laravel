a10e35d84adffb0f42b71232e61fe95fb9d65775
pothea.chorn <pothea.chorn@PNC.LAN>
2020-01-03 09:13:40 +0700

    route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    WEP 2020-A
                 </div>
 
                 <div class="links">
